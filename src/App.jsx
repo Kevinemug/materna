23bc4490c97c6b0c78a84916fe10938a4c7cff2d
Kevinemug <kevineeuwamahoro@gmail.com>
2023-07-15 13:20:45 +0300

    materna header

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+
+function App() {
+
+  return (
+    
+    <>
+    <header className="bg-main w-full text-white md:w-[70%]">
+   <div className="flex space-x-10 py-5 pl-5 md:hidden">
+     <div className="flex space-x-2">
+      <span className="material-symbols-outlined text-xl">menu</span>
+     <div>Menu</div>
+     </div>
+     <div className="flex space-x-2">
+      <div><img  src="https://www.materna.com/SiteGlobals/Frontend/Images/icons/loupe-w.svg?__blob=normal&v=1" alt="" /></div>
+     <div>Search</div>
+     </div>
+   </div>
+   <div className="hidden md:flex  justify-between">
+     <div className="hidden md:flex gap-8 px-5 py-5 text-xl">
+      <div>Portfolio</div>
+      <div>Industries</div>
+      <div>Customers</div>
+      <div>About us</div>
+      <div> Careers</div>
+     </div>
+   <div className=" bg-[#8e1e2b] flex justify-between px-6 items-center"><img  src="https://www.materna.com/SiteGlobals/Frontend/Images/icons/loupe-w.svg?__blob=normal&v=1" alt="" /></div>
+   </div>
+    </header>
+
+
+<main className="bg-hero-pattern">
+
+<div className="w-[100px] h-[100px] p-2">
+  <img src="https://www.materna.com/SiteGlobals/Frontend/Images/logo-small.svg?__blob=normal&v=1" alt="" />
+</div>
+
+
+
+</main>
+
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html",  "./src/**/*.{js,ts,jsx,tsx}",],
+  theme: {
+    extend: {
+      colors:{
+        "main" :"#c30a17"
+          },
+          backgroundImage: {
+            'hero-pattern': "url('https://www.materna.com/SharedDocs/Bilder/DE/buehne_startseite.svg?__blob=panorama&v=1')",
+          }
+      
+     },
+  },
+  plugins: [],
+}
+
